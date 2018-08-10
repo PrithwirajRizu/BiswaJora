fc5ec10785f13ea00fd627080d493f30586662ec
PrithwirajRizu <rizubhattacharjee@gmail.com>
2018-08-10 23:53:55 +0600

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,11 +27,5 @@ class HomeController extends Controller
         return view('home');
     }
 
-    public function isLogin()
-    {
-        if(Auth::check()){
-                    return redirect('/home');
-                }
-                return redirect('/');
-    }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/','HomeController@isLogin');
+
 
 Route::post('/', function () {
     return view('welcome');
